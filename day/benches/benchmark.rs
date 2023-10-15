8c3a0ac4eebc9af7d43cce8685a19c49ba01dae7
Andrew Howard <andrew@adh.me.uk>
2023-10-15 23:29:51 +0100

    include day crate name in benchmark id

--- day/benches/benchmark.rs
+++ day/benches/benchmark.rs
@@ -2,16 +2,16 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use {{crate_name}}::{solve_part1, solve_part2, EXAMPLE, INPUT};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("solve part1 example", |b| {
+    c.bench_function("solve {{crate_name}} part1 example", |b| {
         b.iter(|| solve_part1(black_box(EXAMPLE)));
     });
-    c.bench_function("solve part1 puzzle", |b| {
+    c.bench_function("solve {{crate_name}} part1 puzzle", |b| {
         b.iter(|| solve_part1(black_box(INPUT)));
     });
-    c.bench_function("solve part2 example", |b| {
+    c.bench_function("solve {{crate_name}} part2 example", |b| {
         b.iter(|| solve_part2(black_box(EXAMPLE)));
     });
-    c.bench_function("solve part2 puzzle", |b| {
+    c.bench_function("solve {{crate_name}} part2 puzzle", |b| {
         b.iter(|| solve_part2(black_box(INPUT)));
     });
 }
