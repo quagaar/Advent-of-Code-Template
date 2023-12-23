784d76b3fc8d90739459a9b0e69be6b118ffefe3
Andrew Howard <andrew@adh.me.uk>
2023-12-23 17:45:26 +0000

    Removed bencher

--- day/benches/benchmark.rs
+++ day/benches/benchmark.rs
@@ -1,9 +1,14 @@
-use bencher::setup_benches;
 use criterion::{criterion_group, criterion_main, Criterion};
 use {{crate_name}}::{part1, part2, INPUT};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    setup_benches("{{crate_name}}", &part1::solve, &part2::solve, INPUT, c);
+    c.bench_function("solve {{crate_name}} part1", |b| {
+        b.iter(|| part1::solve(black_box(INPUT)));
+    });
+
+    c.bench_function("solve {{crate_name}} part2", |b| {
+        b.iter(|| part2::solve(black_box(INPUT)));
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
