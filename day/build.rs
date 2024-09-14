ff0a0a73af06aa83a562069319fec8db0041cb96
Andrew Howard <andrew@adh.me.uk>
2024-09-14 18:44:16 +0100

    Fix for build warning introduced in rust v1.80

--- day/build.rs
+++ day/build.rs
@@ -1,12 +1,15 @@
 use std::path::Path;
 
 fn main() {
+    println!("cargo::rustc-check-cfg=cfg(input_txt)");
     if Path::new("input.txt").exists() {
         println!("cargo::rustc-cfg=input_txt");
     }
+    println!("cargo::rustc-check-cfg=cfg(part1_txt)");
     if Path::new("part1.txt").exists() {
         println!("cargo::rustc-cfg=part1_txt");
     }
+    println!("cargo::rustc-check-cfg=cfg(part2_txt)");
     if Path::new("part2.txt").exists() {
         println!("cargo::rustc-cfg=part2_txt");
     }
