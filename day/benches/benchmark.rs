59deaf62f2218edbcac97e2c947d0ab70857f436
Andrew Howard <andrew@adh.me.uk>
2025-11-26 23:42:50 +0000

    Updated criterion version

--- day/benches/benchmark.rs
+++ day/benches/benchmark.rs
@@ -1,4 +1,5 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
 use {{crate_name}}::{part1, part2, INPUT};
 
 fn criterion_benchmark(c: &mut Criterion) {
